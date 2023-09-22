263d8d36c501f9151eec2b753bb0c744125ce557
Paavo Peltonen <paavo@sharkpunch.com>
2023-09-22 17:58:39 +0300

    Add Owner entity

---
+++ src/entity/Owner.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, OneToMany, JoinTable } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Owner {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToMany(() => User, (user: User) => user.owner, { eager: true })
+  @JoinTable()
+  users: User[]
+}
+
